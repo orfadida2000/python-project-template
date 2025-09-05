b5e76471421889b932123cce2ceac1687414be4a
Or Fadida <155828140+orfadida2000@users.noreply.github.com>
2025-09-05 19:44:02 +0300

    v1 updates

---
+++ extensions/faker.py
@@ -0,0 +1,21 @@
+from faker import Faker
+from jinja2.ext import Extension
+
+def name() -> str:
+	en_US_faker = Faker('en_US')
+	return en_US_faker.name()
+
+def paragraph(nb_list: list) -> str:
+	en_US_faker = Faker('en_US')
+	res_str = ""
+	nb_sentences = nb_list[0] if len(nb_list) > 0 else 5
+	nb_words = nb_list[1] if len(nb_list) > 1 else 15
+	for _ in range(nb_sentences):
+		res_str += en_US_faker.sentence(nb_words=nb_words) + "\n"
+	return res_str
+
+class FakerExtension(Extension):
+	def __init__(self, environment):
+		super().__init__(environment)
+		environment.filters["paragraph"] = paragraph
+		environment.filters["name"] = name
